8331f40c801ff0988115141a3bd62dda29611baa
ZhangRan <zran@dhigroup.com>
2023-03-15 16:44:22 +0800

    fix api response issue

--- api/message/index.js
+++ api/message/index.js
@@ -8,6 +8,6 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: { "name": responseMessage }
     };
 }
